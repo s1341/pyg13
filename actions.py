d849b341f6ecf7f1910b35211431c9a9f63a5c8c
srubenst <srubenst@cisco.com>
2014-10-07 09:36:45 +0300

    key press action for osx

--- actions.py
+++ actions.py
@@ -1,6 +1,23 @@
+import platform
+
+
 class Action(object):
-    pass
+    def perform(self, device, key_state):
+        pass
 
+if platform.uname()[0] == "Darwin":
+    class SendKeyAction(Action):
+        def __init__(self, key):
+            # TODO: convert key into OSX virtual key
+            self.key = key
 
-class SendKeyAction(Action):
-    pass
+        def perform(self, device, key_state):
+            import Quartz
+
+            Quartz.CGEventPost(Quartz.kCGHIDEventTap,
+                               Quartz.CGEventCreateKeyboardEvent(None, self.key,
+                                                                 key_state))
+
+elif platform.uname()[0] == "Linux":
+    class SendKeyAction(Action):
+        pass
